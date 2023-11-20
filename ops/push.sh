8d0a2cb703821663889499f7fb1421a4676debd8
charlesDinneya <dinneyacharles007@gmail.com>
2023-11-20 01:26:33 +0100

    messages

--- ops/push.sh
+++ ops/push.sh
@@ -4,5 +4,6 @@ git add .
 git commit -m "$1"
 
 latest=$(cat latest-image.txt)
-latest=$(expr "$latest" + 1)
-echo "$latest"
+updated=$(expr "$latest" + 1)
+
+sed -i '' 's/ropc:$latest/ropc:$updated/g' argo-cd/Deployment.yaml
